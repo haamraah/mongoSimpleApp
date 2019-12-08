a4088845d9405a519dfcc7d2a6069afad6a677af
KuTu <haamraah@yahoo.com>
2019-12-07 19:17:15 -0700

    heroku db

--- server.js
+++ server.js
@@ -26,10 +26,20 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", {
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/unit18Populater";
+
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
 });
+
+
+
+
+// Connect to the Mongo DB
+// mongoose.connect("mongodb://localhost/unit18Populater", {
+//   useNewUrlParser: true,
+// });
 
 // Routes
 
