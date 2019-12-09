e7b0fce4bca25b040890b51c5e3b17d728b45559
KuTu <haamraah@yahoo.com>
2019-12-09 16:18:19 -0700

    heroku db config

--- models/index.js
+++ models/index.js
@@ -3,5 +3,5 @@
 module.exports = {
   Article: require("./Article.js"),
   Note: require("./Note.js")
-  // SavedArticle: require("./SavedArticle.js")
+  SavedArticle: require("./SavedArticle.js")
 };
